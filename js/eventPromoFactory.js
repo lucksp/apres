258121508a570de7f15db5edb220f3b13580b67f
Phil Lucks <philip.lucks@snapengage.com>
2018-01-16 20:51:13 -0700

    formatting

--- js/eventPromoFactory.js
+++ js/eventPromoFactory.js
@@ -1,33 +1,33 @@
 angular.module('apresApp')
-	.factory('eventPromoFactory', ['$http', function($http){
+	.factory('eventPromoFactory', ['$http', function ($http) {
 
-	var eventPromoArray = [];
-	var EventPromoObj = function( id, name, address, city, state, zip, type, category, rating, date, promotion, code, repeat ){
+		var eventPromoArray = [];
+		var EventPromoObj = function (id, name, address, city, state, zip, type, category, rating, date, promotion, code, repeat) {
 
-		this.id				=id;
-		this.name			=name;
-		this.address		=address;
-		this.city			=city;
-		this.state			=state;
-		this.zip			=zip;
-		this.type			=type;
-		this.category		=category;
-		this.rating			=rating;
-		this.date			=date;
-		this.promotion		=promotion;
-		this.code			=code;
-		this.repeat			=repeat;
-		eventPromoArray.push(this)
-		
-	}
+			this.id = id;
+			this.name = name;
+			this.address = address;
+			this.city = city;
+			this.state = state;
+			this.zip = zip;
+			this.type = type;
+			this.category = category;
+			this.rating = rating;
+			this.date = date;
+			this.promotion = promotion;
+			this.code = code;
+			this.repeat = repeat;
+			eventPromoArray.push(this)
+
+		}
 
 		return {
 
-			eventPromoArray	:eventPromoArray,
-			EventPromoObj 	:EventPromoObj,
-				get 		:function(){
-			return $http.get('apresdata.json')
-		}
+			eventPromoArray: eventPromoArray,
+			EventPromoObj: EventPromoObj,
+			get: function () {
+				return $http.get('apresdata.json')
+			}
 
 		}
 
